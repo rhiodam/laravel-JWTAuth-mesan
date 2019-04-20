f3606c8d53b6df4e541c6ff2cb04e4ee475e2ea6
Rhioda muthie <rhiodam@gmail.com>
2019-04-20 12:15:28 +0700

    Step 6: Log User In and Out (*)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -74,14 +74,20 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $credentials = $request->only('email', 'password');
+
         $rules = [
             'email' => 'required|email',
             'password' => 'required',
         ];
+
         $validator = Validator::make($credentials, $rules);
+
         if($validator->fails()) {
-            return response()->json(['success'=> false, 'error'=> $validator->messages()]);
+            return response()->json(['success'=> false, 'error'=> $validator->messages()] , 401);
         }
+
+        $credentials['is_verified'] = 1;
+
         try {
             // attempt to verify the credentials and create a token for the user
             if (!$token = JWTAuth::attempt($credentials)) {
@@ -92,8 +98,9 @@ class AuthController extends Controller
             return response()->json(['success' => false, 'error' => 'Failed to login, please try again.'], 500);
         }
         // all good so return the token
-        return response()->json(['success' => true, 'data'=> [ 'token' => $token ]]);
+        return response()->json(['success' => true, 'data'=> [ 'token' => $token ]] ,200);
     }
+
     /**
      * Log out
      * Invalidate the token, so user cannot use it anymore
@@ -102,7 +109,9 @@ class AuthController extends Controller
      * @param Request $request
      */
     public function logout(Request $request) {
+
         $this->validate($request, ['token' => 'required']);
+
         try {
             JWTAuth::invalidate($request->input('token'));
             return response()->json(['success' => true, 'message'=> "You have successfully logged out."]);
