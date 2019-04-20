43a1c60269b1530192bc2b0e7a680f27a97750b7
Rhioda muthie <rhiodam@gmail.com>
2019-04-20 13:23:16 +0700

    Step 4: Create Models

---
+++ app/Permission.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+use Zizaco\Entrust\EntrustPermission;
+class Permission extends EntrustPermission
+
+//class Permission extends Model
+{
+    //
+}
